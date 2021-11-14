9ba40214f3890226849a5ff2ec2ae4ba9e6a35a9
Daniyal-Arif <daniyalmarif@gmail.com>
2021-11-15 03:50:34 +0500

    javascript portion left

--- src/script.js
+++ src/script.js
@@ -1,3 +1,5 @@
+import L from "../node_modules/leaflet/dist/leaflet";
+
 require("dotenv").config();
 
 const AJAX = async function () {
@@ -10,3 +12,15 @@ const AJAX = async function () {
     console.log(err);
   }
 };
+
+const mymap = L.map("map").setView([51.505, -0.09], 13);
+
+L.tileLayer("https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+}).addTo(mymap);
+
+L.marker([51.5, -0.09])
+  .addTo(mymap)
+  .bindPopup("A pretty CSS3 popup.<br> Easily customizable.")
+  .openPopup();
